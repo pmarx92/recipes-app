35bdc50fd47be425bc9e6fd9b7e5e3392de8f153
Patrick <pamarx92@gmail.com>
2022-10-11 16:37:46 +0200

    removed dummy files, added picture

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,9 +1,22 @@
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import PizzaVeggie from '../public/pizza.jpg'
+
 
 export default function Veggie() {
   return (
     <div className={styles.container}>
     <h1>Hello Veggie</h1>
+
+ <Image
+        src={PizzaVeggie}
+        alt="Picture of the author"
+         width={2400}
+         height={1596} 
+         layout="responsive"
+        // blurDataURL="data:..." automatically provided
+        // placeholder="blur" // Optional blur-up while loading
+      />
     </div>
   )
 }
